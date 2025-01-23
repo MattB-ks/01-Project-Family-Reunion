7303a004f8e7467b81f9c94a67aa35b382718a9c
Doooodbruh <blochmatthew@gmail.com>
2025-01-22 21:01:37 -0600

    Registration JS

--- Assets/JS/Registration.js
+++ Assets/JS/Registration.js
@@ -1,6 +1,7 @@
 const submitEl = document.querySelector('#submit');
 const nameInput = document.querySelector('#name');
 const emailInput = document.querySelector('#email');
+const plus1Input = document.querySelector('#plus1');
 const submissionResponseEl = document.querySelector('#response');
 
 // Action to be performed on click store in named function
@@ -9,7 +10,9 @@ function showResponse(event) {
   event.preventDefault();
   console.log(event);
   const response =
-    'Thank you for your submission ' +
+    'Thank you for registering your party of ' +
+    plus1Input.value +
+    ', ' +
     nameInput.value +
     '! We will reach out to you at ' +
     emailInput.value +
